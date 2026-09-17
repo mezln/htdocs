605c8cea0bcf278b12738b6aefc8af266950c507
Stefan <milovicstefan00@gmail.com>
2026-09-17 14:00:44 +0200

    Delete functionaliteit

--- games/resources/views/games/index.blade.php
+++ games/resources/views/games/index.blade.php
@@ -30,7 +30,7 @@
                         <td>{{ $game->rating }}/10</td>
                         <td> <a href="/games/edit/{{ $game->id }}" class="btn btn-primary btn-sm">Edit</a> </td>
                         <td>
-                            <form action="/games/delete/{{ $game->id }}" method="post">
+                            <form action="/games/destroy/{{ $game->id }}" method="post">
                                 @csrf
                                 <button onclick="return confirm('Weet je het zeker?')" class="btn btn-danger btn-sm" type="submit">Delete</button>
                             </form>
