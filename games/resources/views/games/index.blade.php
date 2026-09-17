c1e64a2f094b77ae8593c22be5b99aead42b4323
Stefan <milovicstefan00@gmail.com>
2026-09-17 13:48:27 +0200

    Update: edit form en update functie

--- games/resources/views/games/index.blade.php
+++ games/resources/views/games/index.blade.php
@@ -17,6 +17,7 @@
                     <th>Game</th>
                     <th>Platform</th>
                     <th>Rating</th>
+                    <th>Edit</th>
                 </tr>
             </thead>
             <tbody>
@@ -25,7 +26,8 @@
                         <td>{{ $game->id }}</td>
                         <td>{{ $game->game_name }}</td>
                         <td>{{ $game->platform }}</td>
-                        <td>{{ $game->rating }}/10</td> 
+                        <td>{{ $game->rating }}/10</td>
+                        <td> <a href="/games/edit/{{ $game->id }}" class="btn btn-primary btn-sm">Edit</a> </td>
                     </tr>
                 @endforeach
             </tbody>
