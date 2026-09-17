e74f3bf7548565add49e5cec2e2f8305489c0232
Stefan <milovicstefan00@gmail.com>
2026-09-17 13:26:08 +0200

    ik heb een  create en save function aangemaakt

--- games/app/Http/Controllers/GameController.php
+++ games/app/Http/Controllers/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
@@ -20,16 +21,32 @@ class GameController extends Controller
      */
     public function create()
     {
-        //
+        return view('games.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
+{
+    $request->validate([
+        'game_name' => 'required',
+        'platform' => 'required',
+        'genre' => 'required',
+        'rating' => 'required|numeric|min:0|max:10'
+    ]);
+
+    $game = new Game([
+        'game_name' => $request->get('game_name'),
+        'platform' => $request->get('platform'),
+        'genre' => $request->get('genre'),
+        'rating' => $request->get('rating')
+    ]);
+
+    $game->save();
+
+    return redirect('/games')->with('success', 'Game added!');
+}
 
     /**
      * Display the specified resource.
